3a9d1058d4c3902cd9edb27ceafd54d9ed8ce270
Itaka <yuyu95125@gmail.com>
2024-02-21 23:15:23 +0900

    add func mod to test.go

--- test.go
+++ test.go
@@ -8,7 +8,14 @@ func calc(x, y int) int {
 	return z
 }
 
+func mod(x, y int) int {
+	var z int
+	z = x % y
+	return z
+}
+
 func main() {
 	fmt.Print("hello")
 	fmt.Print(calc(1, 2))
+	fmt.Print(mod(1, 2))
 }
