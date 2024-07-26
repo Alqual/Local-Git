87dc88e3432226195dc35db18b4b6e48a770bd29
Alqual <99069789+Alqual@users.noreply.github.com>
2024-07-26 23:15:09 +0900

    review on 2315 at Jul26

--- Except-Atcoder/Secure test/ABC080_review_official.go
+++ Except-Atcoder/Secure test/ABC080_review_official.go
@@ -8,6 +8,9 @@ import (
 	"strings"
 )
 
+// codeのタイプによって、コードの可用性は異なって良い。今回のは本番用なので、メンテナンス性を重視したレビューとしてる
+
+
 var Buf_io = bufio.NewScanner(os.Stdin) // what means this operation?
 // more describtive name is better, Scan_start is a little simple
 const Buf_init_size = 10000
@@ -38,6 +41,7 @@ func main() {
 	var Profit_shops [][]int= [][]int{} //[shopid, sink running zones]
 	//Shortcut form (in this case, F and P are parallelly called) is difficult?
 	var ans, co, min int = 0, 0, 0 // Is this op really necessary?
+	var Max_profit_array []int = []int{}
 	var Max_profit int = 0
 
 	co2 := make([]int, N) //??? corbon dioxide?
@@ -54,34 +58,24 @@ func main() {
 
 	fmt.Println(Shop_run_time, Profit_shops, Shop_run_time[0][1]) // is matrix necessary? normally matrix is more cost
 	for timezone := 0; timezone < 10; timezone++ {
-		if timezone == 0 {
-			for j := 0; j < N; j++ {
-				Max_profit += Profit_shops[0][j] // ans is too simple. what means about the val?
-			}
-		} else {
-			d := 0
-			for j := 0; j < N; j++ { // other loop, using "j" is no so much bad, but even though a little not clear
+			for shops := 0; shops < N; shops++ { // other loop, using "j" is no so much bad, but even though a little not clear
 		
-				if Shop_run_time[j][timezone] == 1 {
-					d += Profit_shops[j][timezone]
+				if Shop_run_time[shops][timezone] == 1 {
+					Max_profit_array[timezone] += Profit_shops[shops][timezone]
 				}
 			}
-			if d > 0 && d> {// is this necessary?
-				Max_profit += d
-				co++
-			} else {
-				if min == 0 {
-					min = d
-				} else {
-					if min < d {
-						min = d
-					}
-				}
-			}
-			if co == 0 {
-				Max_profit += min
-			}
-		}
 	}
+
+//Arrange Max_profit_array by disorder
+for timezone := 0; timezone < 10; timezone++ {
+	if timezone == 0 && Max_profit_array[timezone] <0{
+		Max_profit = Max_profit_array[0]
+		break
+	}
+	if Max_profit_array[timezone]=<0{
+		break
+	}
+	Max_profit+=Max_profit_array[timezone]
+}
 	fmt.Println(Max_profit)
 }
