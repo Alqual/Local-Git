0daf828f25651971fcf807662e4a040761dd0e88
Alqual <99069789+Alqual@users.noreply.github.com>
2024-07-26 12:27:09 +0900

    check 1 at Jul26

--- Except-Atcoder/Secure test/ABC080_review.go
+++ Except-Atcoder/Secure test/ABC080_review.go
@@ -8,12 +8,13 @@ import (
 	"strings"
 )
 
-var sc = bufio.NewScanner(os.Stdin) // what means this operation?
+var Scan_start = bufio.NewScanner(os.Stdin) // what means this operation?
+// more describtive name is better, Scan_start is a little simple
 var buffer = make([]byte, 10000)
 
-func Newline() []int { // function vals are OK for this simple names?
-	sc.Scan()
-	arr := strings.Split(sc.Text(), " ")
+func Input_List() []int { // function vals are OK for this simple names?
+	Scan_start.Scan()
+	arr := strings.Split(Scan_start.Text(), " ") // arr is not good ?
 	ret := make([]int, len(arr))
 	for i, v := range arr {
 		ret[i], _ = strconv.Atoi(v)
@@ -21,22 +22,21 @@ func Newline() []int { // function vals are OK for this simple names?
 	return ret
 }
 
-func NewInt() int {
-	sc.Scan()
+func Input_Int() int {
+	Scan_start.Scan()
 	ret, _ := strconv.Atoi(sc.Text())
 	return ret
 }
 
 func main() {
-	N := NewInt() //NewInt is not clear. should be more describetive name
+	N := Input_Int() //NewInt is not clear. should be more describetive name
 	var F, P [][]int = [][]int{}, [][]int{} // what is F, and P ?
+	//Shortcut form (in this case, F and P are parallelly called) is difficult?
 	var ans, co, min int = 0, 0, 0 // Is this op really necessary?
 	co2 := make([]int, N) //??? corbon dioxide?
 	for i := 0; i < N; i++ { // i is not clear. what it is for? counting some?		F = append(F, Newline())
-	}
-	for i := 0; i < N; i++ { //same issues as the last one
 		// same loop vals makes confusion which gives loops
-		P = append(P, Newline())
+		P = append(P, Input_List())
 	}
 	fmt.Println(F, P, F[0][1]) // is matrix necessary? normally matrix is more cost
 	for i := 0; i < 10; i++ {
@@ -46,7 +46,8 @@ func main() {
 			}
 		} else {
 			d := 0
-			for j := 0; j < N; j++ {
+			for j := 0; j < N; j++ { // other loop, using "j" is no so much bad, but eve
+		
 				if F[j][i] == 1 {
 					d += P[j][i]
 				}
