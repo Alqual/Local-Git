bcd5d4f7dd20b147339247a7b5bd2a7e0f8b8b6d
Alqual <tack71088@gmail.com>
2024-08-14 09:05:25 +0900

    update

---
+++ Except-Atcoder/Secure test/ABC057_Dsecure.go
@@ -0,0 +1,58 @@
+//coding review: Jul 26,2024
+// url: https://atcoder.jp/contests/abc057/tasks/abc057_d
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var buffer = make([]byte, 10000)
+
+func Newline() []int {
+	sc.Scan()
+	ret := strings.Split(sc.Text(), " ")
+	var reti []int
+	for _, s := range ret {
+		res, _ := strconv.Atoi(s)
+		reti = append(reti, res)
+	}
+	return reti
+}
+
+func main() {
+	W := Newline() //N A B
+	V := Newline() //v1 v2 ... vn
+	sort.Ints(V)
+
+	var sum, cns, cns2 int
+	//var sumweight int
+	//var maxaverageset int
+	for i := W[0] - 1; i >= 0; i-- {
+		if i > W[0]-W[1] {
+			sum += V[i]
+			cns2++
+		} else if i == W[0]-W[1] {
+			sum += V[i]
+			cns++
+			cns2++
+		} else {
+			if (float64(sum)/float64(cns2) == float64(sum+V[i])/float64(cns2+1)) && cns2 <= W[2] {
+				sum += V[i]
+				cns2++
+				cns++
+			} else if V[i] == V[W[0]-W[1]] {
+
+			} else {
+				break
+			}
+		}
+	}
+	fmt.Println(float64(sum)/float64(cns2), cns)
+}
